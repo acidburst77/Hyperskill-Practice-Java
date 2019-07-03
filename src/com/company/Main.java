c68890dab3d4d58d7df1a7d1e3815d72b10942a1
varaksina.a <varaksina.a@ladyagroup.ru>
2019-07-03 09:49:58 +0300

    Interesting array of strings printing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 	    //The sum of elements
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         int number = 0;
         int sum = 0;
         do {
@@ -12,6 +12,29 @@ public class Main {
             sum += number;
         } while (number != 0);
 
-        System.out.println(sum);
+        System.out.println(sum);*/
+
+        Scanner sc = new Scanner(System.in);
+
     }
 }
+
+// Интересный вывод массива строк
+/*class Application {
+    String name;
+    public void run(String[] str) {
+        System.out.println(this.name);
+
+        for(int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+    }
+
+    public void run(String[] str) {
+        System.out.println(this.name);
+
+        for(String item : str) {
+            System.out.println(item);
+        }
+    }
+}*/
