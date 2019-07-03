5c5fc833b18be239de88c75c94c1161529eb59ef
varaksina.a <varaksina.a@ladyagroup.ru>
2019-07-03 11:49:25 +0300

    working witn enums

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,99 @@
 package com.company;
 import java.util.*;
 
+enum Season {
+    SPRING, SUMMER, AUTUMN, WINTER
+}
+
+enum UserStatus {
+    PENDING, ACTIVE, BLOCKED
+}
+
+enum TransactionStatus {
+    WAITING(false),
+    PROCESSING(false),
+    COMPLETED(false),
+    ERROR(false);
+
+    private boolean done;
+
+    TransactionStatus(boolean done) {
+        this.done = done;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+}
+
+enum Secret {
+    STAR, CRASH, START
+}
+
+enum Direction {
+    EAST("E"),
+    WEST("W"),
+    NORTH("N"),
+    SOUTH("S");
+
+    private final String shortCode;
+
+    Direction(String code) {
+        this.shortCode = code;
+    }
+
+    public String getShortCode() {
+        return this.shortCode;
+    }
+}
+
+enum Month {
+    JAN(1),
+    FEB(2),
+    MAR(3),
+    APR(4),
+    MAY(5),
+    JUN(6),
+    JUL(7),
+    AUG(8),
+    SEP(9),
+    OCT(10),
+    NOV(11),
+    DEC(12);
+
+    Month(int number) {
+        this.number = number;
+    }
+
+    private final int number;
+
+    public int getNumber() {
+        return number;
+    }
+}
+
+enum Planet {
+
+    VENUS   (4.869e+24, 6.0518e+6),
+    EARTH   (5.976e+24, 6.37814e+6),
+    MARS    (6.421e+23, 3.3972e+6),
+    JUPITER (1.9e+27,   7.1492e+7),
+    SATURN  (5.688e+26, 6.0268e+7),
+    URANUS  (8.686e+25, 2.5559e+7);
+
+    private final double m; // mass in kilograms
+    private final double r; // radius in meters
+
+    Planet(double mass, double radius) {
+        this.m = mass;
+        this.r = radius;
+    }
+
+    public double mass() { return m; }
+
+    public double radius() { return r; }
+}
+
 public class Main {
     public static void main(String[] args) {
 	    //The sum of elements
@@ -15,10 +108,65 @@ public class Main {
         System.out.println(sum);*/
 
         // Enums
-        enum Season {
-            SPRING, SUMMER, AUTUMN, WINTER
+        /*Season summer = Season.SUMMER;
+        Season autumn = Season.AUTUMN;
+
+        System.out.println(summer);
+        System.out.println(summer.equals(Season.SUMMER));
+        System.out.println(summer == Season.SUMMER);
+
+        System.out.println(autumn.equals(Season.AUTUMN));
+        System.out.println(autumn == Season.AUTUMN);
+
+        Season[] seasons = Season.values();
+        System.out.println(seasons);
+
+        Season season = Season.valueOf("AUTUMN");
+        System.out.println(season);
+
+        String winter = Season.WINTER.name();
+        String autumn2 = Season.AUTUMN.toString();
+        System.out.println(winter);
+        System.out.println(autumn2);
+
+        UserStatus status = UserStatus.valueOf("ACTIVE");
+        switch(status) {
+            case PENDING:
+                System.out.println("You need to wait a little");
+                break;
+            case ACTIVE:
+                System.out.println("No problems, you may pass here");
+                break;
+            case BLOCKED:
+                System.out.println("Stop! You can't pass here");
+                break;
+            default:
+                System.out.println("Unsupported enum constant");
         }
 
+        TransactionStatusDemo st = new TransactionStatusDemo();
+        st.printFullStatus(TransactionStatus.COMPLETED);*/
+
+        // Counting enums that contains string "STAR"
+        /*int count = 0;
+        for(Secret item : Secret.values()) {
+            if(item.toString().startsWith("STAR")) {
+                count++;
+            }
+        }
+        System.out.println(count);*/
+
+        /*for (Planet planet : Planet.values()) {
+            if(planet.mass() > 5.0e+24 && planet.radius() > 6.0e+7) {
+                System.out.println(planet);
+            }
+        }*/
+    }
+}
+
+class TransactionStatusDemo {
+    void printFullStatus(TransactionStatus status) {
+        System.out.println("Status: " + status + ", is finished: " + status.isDone());
     }
 }
 
